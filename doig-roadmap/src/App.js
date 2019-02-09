fcfe3401adfe5015bdbd6cc136c6da7be3f495bc
Teyler Halama <teyler.halama@gmail.com>
2019-02-09 11:11:59 -0700

    Started working on markers

--- doig-roadmap/src/App.js
+++ doig-roadmap/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 console.log(`${process.env.REACT_APP_GAPI_KEY}`);
 
@@ -9,17 +9,49 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+    onClose = props => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        });
+      }
+    };
   render() {
     return (
       <Map
         google={this.props.google}
-        zoom={4}
-        style={mapStyles}
-        initialCenter={{
-         lat: 37.09024,
-         lng: -95.712891
-        }}
-      />
+        zoom={14}
+        style={style}
+        initialCenter={{ lat: -1.2884, lng: 36.8233 }
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
