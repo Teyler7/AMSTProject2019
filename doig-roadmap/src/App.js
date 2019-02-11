6560a306d4109737d8a6510ee06c782f31ba2a69
Teyler Halama <teyler.halama@gmail.com>
2019-02-11 11:31:04 -0700

    Cleaned code with Header.jsx and LocaitonsList.jsx

--- doig-roadmap/src/App.js
+++ doig-roadmap/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import Swal from 'sweetalert2'
 import './App.scss';
+
+import Header from "./components/Header"
+import LocationsList from "./components/LocationsList"
 
 const data = require('./markerData.json');
 
@@ -42,56 +43,11 @@ export class MapContainer extends Component {
     }
   };
 
-  onAddButtonClick = props => {
-    Swal.mixin({
-      input: 'text',
-      confirmButtonText: 'Next &rarr;',
-      showCancelButton: true,
-      progressSteps: ['1', '2', '3', '4', '5']
-    }).queue([
-      {
-        title: 'Location Name',
-        text: 'Enter the name of the location to add to the map'
-      },
-      {
-        title: 'Latitude',
-        text: 'Enter the latitude of the location'
-      },
-      {
-        title: 'Longitude',
-        text: 'Enter the longitude of the location'
-      },
-      {
-        title: 'Date',
-        text: 'Enter the date of the resource'
-      },
-      {
-        title: 'Meta Data',
-        text: 'Enter some meta data about this resource'
-      }
-
-    ]).then((result) => {
-      if (result.value) {
-        Swal.fire({
-          title: 'Success!',
-          type: 'success',
-          html:
-            'Your answers: <pre><code>' +
-              JSON.stringify(result.value) +
-            '</code></pre>',
-          confirmButtonText: 'Lovely!'
-        });
-      }
-    })
-  }
-
   render() {
     return (
-      <div>
-        <div className="header">
-          <div><h2 className="title">Doig Roadmap</h2></div>
-          <div className="addButton" onClick={this.onAddButtonClick}><FontAwesomeIcon icon={faMapMarkerAlt} size="2x"/></div>
-        </div>
+      <div className="map">
+        <Header></Header>
+        <LocationsList></LocationsList>
       <Map
         google={this.props.google}
         zoom={4}
---
+++ doig-roadmap/src/components/LocationsList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.scss';
+
+export default class LocationsList extends Component {
+  render() {
+    return (
+        <div className="list">
+            <h3>Locations</h3>
+        </div> 
+    );
+  }
+}
