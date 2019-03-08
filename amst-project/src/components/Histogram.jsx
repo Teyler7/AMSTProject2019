a7ae9bafef29d213fa2a92a8a9e1bb4788d99431
Teyler Halama <teyler.halama@gmail.com>
2019-03-07 17:43:43 -0700

    Added era Rect Series behind line graph

--- amst-project/src/components/Histogram.jsx
+++ amst-project/src/components/Histogram.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import '../../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis} from 'react-vis';
+import {XYPlot, LineSeries, VerticalRectSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis} from 'react-vis';
+
+const DATA = [
+  {x0: 1900, x: 1912, y: 4000},
+  {x0: 1940, x: 1920, y: 4000}
+]
 
 export default class Histogram extends Component {
   render() {
@@ -11,7 +16,8 @@ export default class Histogram extends Component {
             <HorizontalGridLines />
             <XAxis />
             <YAxis />
-            <LineSeries data={this.props.data.plot} />
+            <VerticalRectSeries data={this.props.data.eras} color={"blue"}/>
+            <LineSeries data={this.props.data.plot}/>
         </XYPlot>
       </div>
     );
