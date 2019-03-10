08ce757934e491c0c3250eef11fceb803e3c52e3
Dylan Rosenbaum <dylankr@live.com>
2019-03-10 00:44:30 -0600

    added my stuff

--- amst-project/src/App.js
+++ amst-project/src/App.js
@@ -1,20 +1,72 @@
-import React, { Component } from 'react';
-import './sass/App.scss';
+import React, { Component } from "react";
+import "./sass/App.scss";
+import pic from "./data/p16013coll27_428_medium.jpg";
+import data from "./data/canningData.json";
 
-import Header from "./components/Header"
-import List from "./components/List"
-import Histogram from './components/Histogram';
-const data = require('./data/canningData.json');
+import Header from "./components/Header";
+//import List from "./components/List"
+import Histogram from "./components/Histogram";
+//const data = require('./data/canningData.json');
 
 export default class App extends Component {
+  state = {
+    index: null
+  };
+
+  handleClick = eraId => {
+    this.setState({ index: eraId });
+  };
 
   render() {
     return (
       <div>
-        <Header></Header>
-        {/* <List markerData={this.state.}></List> */}
-        <Histogram data={data}></Histogram>
+        <Header />
+        <div className="container px-sm">
+          <div className="row">
+            <div className="col-1" />
+            <div className="col">
+              <h1>Story of Cans</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h2>
+                <u>Eras</u>
+              </h2>
+              <ul>
+                {data.eras.map(era => (
+                  <li>
+                    <button
+                      onClick={() => this.handleClick(era.id)}
+                      key={era.id}
+                      className="btn btn-outline-success m-2"
+                    >
+                      {era.eraName}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="col">
+              <img src={pic} alt="" />
+            </div>
+          </div>
+          {/* <List markerData={this.state.}></List> */}
+          <div className="row">
+            <Histogram data={data} />
+          </div>
+          <div className="row">
+            <div className="col">
+              <p>{this.handleInfo()}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
+  }
+  handleInfo() {
+    if (this.state.index != null) {
+      return data.eras[this.state.index].information;
+    }
   }
 }
--- amst-project/src/components/Histogram.jsx
+++ amst-project/src/components/Histogram.jsx
@@ -1,38 +1,55 @@
-import React, { Component } from 'react';
-import '../../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries, VerticalRectSeries, VerticalGridLines, Crosshair, HorizontalGridLines, XAxis, YAxis} from 'react-vis';
+import React, { Component } from "react";
+import "../../node_modules/react-vis/dist/style.css";
+import {
+  XYPlot,
+  LineSeries,
+  VerticalRectSeries,
+  VerticalGridLines,
+  Crosshair,
+  HorizontalGridLines,
+  XAxis,
+  YAxis
+} from "react-vis";
 
 export default class Histogram extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          crosshairValues: [""],
-          index: 0
-        };
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      crosshairValues: [""],
+      index: 0
+    };
+  }
   render() {
     return (
       <div className="histogram-wrapper">
-        <XYPlot height={300} width= {1200}>
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis />
-            <YAxis />
-            <VerticalRectSeries data={this.props.data.eras} colorRange={[/*From Red*/"#ff0000", "#0000ff"/*To Blue*/]}  opacity={.45}/>
-            <LineSeries color={"black"} data={this.props.data.plot}
-                onNearestX={(value, {index}) =>{
-                    console.log(index)
-                    this.setState({crosshairValues: this.props.data.plot.map(d => d)})
-                    this.setState({index: index})
-                    console.log(this.state)}
-                }
-            />
-            <Crosshair values={this.state.crosshairValues}>
-                    <div style={{ background: 'black' }}>
-                        <h2>Year: {this.state.crosshairValues[this.state.index].x}</h2>
-                        <h2>Count: {this.state.crosshairValues[this.state.index].y}</h2>
-                    </div>
-                </Crosshair>
+        <XYPlot height={300} width={1100}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <VerticalRectSeries
+            data={this.props.data.eras}
+            colorRange={[/*From Red*/ "#ff0000", "#0000ff" /*To Blue*/]}
+            opacity={0.45}
+          />
+          <LineSeries
+            color={"black"}
+            data={this.props.data.plot}
+            onNearestX={(value, { index }) => {
+              console.log(index);
+              this.setState({
+                crosshairValues: this.props.data.plot.map(d => d)
+              });
+              this.setState({ index: index });
+              console.log(this.state);
+            }}
+          />
+          <Crosshair values={this.state.crosshairValues}>
+            <div style={{ background: "black" }}>
+              <h2>Year: {this.state.crosshairValues[this.state.index].x}</h2>
+              <h2>Count: {this.state.crosshairValues[this.state.index].y}</h2>
+            </div>
+          </Crosshair>
         </XYPlot>
       </div>
     );
